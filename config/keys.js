fa318e6731071fe9a04858888369925468e22d20
ManikantaDema <d.manikanta1234@gmail.com>
2018-06-05 15:27:04 -0400

    devconnector at the end

---
+++ config/keys.js
@@ -0,0 +1,13 @@
+// if (process.env.NODE_ENV === "production") {
+//   module.export = require("./keys_prod");
+// } else {
+//   module.export = require("./keys_dev");
+// }
+
+module.exports = {
+  mongoURI: 'mongodb://devconnectors:mani4321@ds249530.mlab.com:49530/devconnectors',
+  secretOrKey: 'secret'
+};
+
+//mongodb://localhost:27017/devconnector
+//mongodb://manikanta.dema:mani4321@ds249530.mlab.com:49530/devconnectors
